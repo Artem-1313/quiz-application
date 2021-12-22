66a776a3c4eb8127f467791b5c792295ea47367a
artem <artemkhmil@ukr.net>
2021-12-22 18:25:36 +0200

    add checkbox and radio buttons for qui form

--- qapp/views.py
+++ qapp/views.py
@@ -20,18 +20,16 @@ class QuizDetail(DetailView):
     model = Quiz
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
         quiz = Quiz.objects.get(id=self.kwargs['pk'])
         questions_list = []
         for q in quiz.get_questions():
+            print(type(q))
             answers = []
             for a in q.get_answers():
                 answers.append(str(a))
-            questions_list.append({str(q): answers})
+            questions_list.append({q: answers})
 
-        # print("request is", self.request.POST)
         context['dict_questions_answers'] = questions_list
         return context
 
